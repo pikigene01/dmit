01c3f51da123d3f43fbb49a3e84885c4a02f7458
Gene Piki <pikigene01@gmail.com>
2023-03-12 14:50:59 +0200

    update direct message icons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+import { contacts_icon_img, messages_icon_img, settings_icon_img, websites_icon_img } from "../services/icons";
 
 export default function Main() {
+  const [appData, setAppData] = useState({
+    search: "",
+  });
+
+  const handleChange = (e) => {
+    setAppData({ ...appData, [e.target.name]: e.target.value });
+  };
   return (
-    <div className='main_app'>
-    <div className='flex_end'>
+    <div className="main_app">
+      <div className="flex_end">
         <div>Direct Message</div>
         <div>minimize,full</div>
+      </div>
+      <div className="gridTwo">
+        <div className="sidebar">
+          <div className="search_head">
+            <div className="search_input">
+              <input
+                type="text"
+                value={appData?.search}
+                onChange={handleChange}
+                name="search"
+                placeholder="search"
+              />
+              <img src={websites_icon_img} className="website icon" alt="search" />
+              <span></span>
+            </div>
+            <div className="head_settings">
+              <img src={settings_icon_img} className="settings" alt="settings" />
+            </div>
+          </div>
+          <div className="tabs">
+            <span className="tab active">
+              <img src={messages_icon_img} className="chats icon" alt="messages" />
+              <p>Chats</p>
+            </span>
+            <span className="tab">
+              <img src={contacts_icon_img} className="contacts icon" alt="contacts" />
+              <p>Contacts</p>
+
+            </span>
+          </div>
+        </div>
+        <div className="dm_msgs">Messages Wrapper</div>
+      </div>
     </div>
-    <div className='gridTwo'>
-        <div className='sidebar'>Contacts</div>
-        <div className='dm_msgs'>Messages Wrapper</div>
-    </div>
-    
-    </div>
-  )
+  );
 }
---
+++ src/services/icons.js
@@ -0,0 +1,10 @@
+import settings_icon from "../img/icons/dots-menu2.png";
+import contacts_icon from "../img/icons/contacts.png";
+import messages_icon from "../img/icons/discuss.png";
+import websites_icon from "../img/icons/website.png";
+
+
+export const settings_icon_img = settings_icon;
+export const contacts_icon_img = contacts_icon;
+export const messages_icon_img = messages_icon;
+export const websites_icon_img = websites_icon;
